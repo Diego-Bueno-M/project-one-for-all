da5d55571339d614c1f8eb37971b3ab79f5d9701
Diego Bueno <diegobuenomendonca@hotmail.com>
2022-08-02 18:25:09 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +11,19 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+const getTalkers = async () => {
+  const talkers = JSON.parse(await fs.readFile('./talker.json', 'utf8'));
+  return talkers;
+};
+
+app.get('/talker', async (req, res) => {
+    const talkers = await getTalkers();
+    if (!talkers) {
+      return res.status(200).json([]);
+    }
+    res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
